b0ce12a87d53fbce5746d86d44b1a877534f13b0
Cece Zinny <cwzinny@gmail.com>
2018-07-17 15:55:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+configure do 
+  set :views, "views"
+  set :public_dir, "public"
+end 
+
 	get '/' do
-		
+		erb :index 
+		# says when i am at my home page, i am going to erb 
 	end
+	
+	get '/info' do 
+	  erb :info
+	end 
+	
 end
