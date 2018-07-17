ce2d5caa1971640e57a66d801473a88d1974630d
Hope Zouzas <hmzouzas@gmail.com>
2018-07-17 15:55:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  configure do
+    set :views, "views"
+set :public_air, "public"
+end 
 	get '/' do
-		
-	end
+	  erb :index 
+end
+
+	get '/info' do 
+	 erb :info
+	end 
+	
+	
 end
